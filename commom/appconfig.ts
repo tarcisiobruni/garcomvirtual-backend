ab779cc82910dd6bd6b5fc91074ba248a3c55206
tarcisiobruni <tarcisiobruni@hotmail.com>
2019-10-03 17:36:47 -0300

    fix(porta): corrige o nome da variavel de ambiente para expor com default do heroku

--- commom/appconfig.ts
+++ commom/appconfig.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
     url: (process.env.API_URL || 'http://localhost'),
-    port: (process.env.API_PORT || '3000'),
+    port: (process.env.PORT || '3000'),
     env: (process.env.APP_ENV || 'dev')
 };
